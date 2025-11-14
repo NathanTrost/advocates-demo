bb259f70c16cafb26a297dcadb3fdcf044a06788
Nathan Trost <n.d.trost@gmail.com>
2025-11-14 15:14:36 -0600

    Style and componentize ExpandableTable

---
+++ src/app/components/formatPhoneNumber.util.ts
@@ -0,0 +1,16 @@
+/** Simple util to validate and format a 10 digit number into a pretty phone number.
+ * This does not validate number criteria outside of digit count.
+ */
+export const formatPhoneNumber = (phoneNumber?: number): string => {
+  if (!phoneNumber) {
+    return '(___) ___-____';
+  }
+
+  const phoneStr = String(phoneNumber);
+  const match = phoneStr.match(/^(\d{3})(\d{3})(\d{4})$/);
+
+  if (match) {
+    return '(' + match[1] + ') ' + match[2] + '-' + match[3];
+  }
+  return phoneStr;
+};
